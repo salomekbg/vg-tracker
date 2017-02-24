fd2637b769e3f8bd2226e2793a450296706c9e59
SluvsP <salomekbg@gmail.com>
2017-02-24 18:01:30 -0500

    create create user method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,13 @@ class UsersController < ApplicationController
   end
 
   def create
+    byebug
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
 
   def show
@@ -20,5 +27,11 @@ class UsersController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email_address, :password)
   end
 end
