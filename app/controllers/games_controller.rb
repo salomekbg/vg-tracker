0463ec2f682ba7dc695f96e53826e03d034a2a18
SluvsP <salomekbg@gmail.com>
2017-03-05 14:07:10 -0500

    temporarily remove checking validation in game create method in game_controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -9,11 +9,8 @@ class GamesController < ApplicationController
 
   def create
     @game = Game.new(game_params)
-    if @game.save
-      redirect_to @game
-    else
-      render 'new'
-    end
+    @game.save
+    redirect_to @game
   end
 
   def show
