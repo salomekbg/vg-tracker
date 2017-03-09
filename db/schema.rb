372abbfcf518271ddc56a30ff1b14e9133eb80ee
SluvsP <salomekbg@gmail.com>
2017-03-09 16:47:58 -0500

    add ability to edit game title

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170225003256) do
+ActiveRecord::Schema.define(version: 20170309213857) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,9 +24,9 @@ ActiveRecord::Schema.define(version: 20170225003256) do
 
   create_table "games", force: :cascade do |t|
     t.string   "title"
+    t.integer  "year"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer  "year"
   end
 
   create_table "genres", force: :cascade do |t|
@@ -63,9 +63,9 @@ ActiveRecord::Schema.define(version: 20170225003256) do
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "email_address"
-    t.string   "password"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
 end
