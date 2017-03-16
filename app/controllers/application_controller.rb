7a60c772eff7fbb27547330cee9bc0c19462d05d
SluvsP <salomekbg@gmail.com>
2017-03-16 16:56:22 -0400

    add user login check and redirect if not logged in, testing with games controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def require_login
+    redirect_to '/signin' unless current_user
+  end
 end
--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,6 @@
 class GamesController < ApplicationController
+  before_action :require_login
+
   def index
     @games = Game.all
   end
