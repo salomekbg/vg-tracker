f84077ed1e05d2f1acf5a7246333bd8a55fdb420
SluvsP <salomekbg@gmail.com>
2017-03-20 14:33:18 -0400

    fix issue with redirection when creating new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :require_login, only: [:index, :new, :show]
+  before_action :require_login, only: [:index, :show, :edit]
 
   def index
     @users = User.all
