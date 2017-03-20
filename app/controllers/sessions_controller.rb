484ae2aff7ca79dd8f28753246d8b30013a82d71
SluvsP <salomekbg@gmail.com>
2017-03-20 18:03:26 -0400

    create validation for signing in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,17 @@
 class SessionsController < ApplicationController
 
   def new
+    @error = ""
   end
 
   def create
+    @error = ""
     @user = User.find_by_username(params[:session][:username])
     if @user && @user.authenticate(params[:session][:password])
       session[:user_id] = @user.id
       redirect_to @user
     else
+      @error = "Sorry, either your username or password is wrong or you don't have an account setup! "
       render 'new'
     end
   end
