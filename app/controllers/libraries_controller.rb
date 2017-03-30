b6acc36d105b80038e7fb648b9a0e40c75e82ba9
SluvsP <salomekbg@gmail.com>
2017-03-30 13:55:05 -0400

    add button on show game page to add that specific game to the library, create library new form with dropdowns and passing game name and id through, create ability to save new library and redirect to user show page

--- app/controllers/libraries_controller.rb
+++ app/controllers/libraries_controller.rb
@@ -1,4 +1,26 @@
 class LibrariesController < ApplicationController
   before_action :require_login
 
+  def new
+    @library = Library.new
+    @games = Game.all
+    @platforms = Platform.all
+    @sources = Source.all
+  end
+
+  def create
+    @library = Library.new(library_params)
+    @user = User.find(current_user.id)
+    if @library.save
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def library_params
+    params.require(:library).permit(:platform_id, :source_id, :status, :personal_rating, :hours_played, :notes).merge(user_id: current_user.id, game_id: params[:game_id])
+  end
 end
