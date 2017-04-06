0bebb281378d509882d427805b979513747a931c
SluvsP <salomekbg@gmail.com>
2017-04-06 10:13:25 -0400

    remove url field from API

--- app/adapters/igdb_api.rb
+++ app/adapters/igdb_api.rb
@@ -1,7 +1,7 @@
 class IgdbApi
 
   def self.get_games(term)
-    results = Unirest.get "https://igdbcom-internet-game-database-v1.p.mashape.com/games/?fields=name%2Curl%2Csummary%2Cstoryline%2Cgame_modes%2Cthemes%2Cgenres%2Crelease_dates%2Cscreenshots%2Cvideos%2Ccover&limit=20&offset=0&search=#{term}",
+    results = Unirest.get "https://igdbcom-internet-game-database-v1.p.mashape.com/games/?fields=name%2Csummary%2Cstoryline%2Cgame_modes%2Cthemes%2Cgenres%2Crelease_dates%2Cscreenshots%2Cvideos%2Ccover&limit=20&offset=0&search=#{term}",
     headers:{
       "X-Mashape-Key" => "LC6dJEpGZVmshUoieKLHL6Gumqwwp1rTJmvjsnJhpSSyDsSUd7",
       "Accept" => "application/json"
