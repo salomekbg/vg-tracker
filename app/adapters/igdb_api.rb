ad8651cf0935e26ca9b23f887aac6668fc4615bb
SluvsP <salomekbg@gmail.com>
2017-04-10 11:09:32 -0400

    update get_games method in igdb adapter to also create game instance with summary and cover, update same method with new variable games, instead of results

--- app/adapters/igdb_api.rb
+++ app/adapters/igdb_api.rb
@@ -1,17 +1,17 @@
 class IgdbApi
 
   def self.get_games(term)
-    results = Unirest.get "https://igdbcom-internet-game-database-v1.p.mashape.com/games/?fields=name%2Csummary%2Cgame_modes%2Cthemes%2Cgenres%2Crelease_dates%2Ccover&limit=20&offset=0&search=#{term}",
+    games = Unirest.get "https://igdbcom-internet-game-database-v1.p.mashape.com/games/?fields=name%2Csummary%2Cgame_modes%2Cthemes%2Cgenres%2Crelease_dates%2Ccover&limit=20&offset=0&search=#{term}",
     headers:{
       "X-Mashape-Key" => "LC6dJEpGZVmshUoieKLHL6Gumqwwp1rTJmvjsnJhpSSyDsSUd7",
       "Accept" => "application/json"
     }
 
-    results.body.each do |result|
-      if result["release_dates"]
-        Game.find_or_create_by(name: result["name"], year: result["release_dates"][0]["human"][0..3])
+    games.body.each do |game|
+      if game["release_dates"]
+        Game.find_or_create_by(name: game["name"], year: game["release_dates"][0]["human"][0..3], summary: game["summary"], cover: game["cover"])
       else
-        Game.find_or_create_by(name: result["name"], year: "No Year Available")
+        Game.find_or_create_by(name: game["name"], year: "No Year Available")
       end
     end
   end
