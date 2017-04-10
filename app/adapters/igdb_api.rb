65459dae4c6ed1d0e0f7733271f25ca619d567a1
SluvsP <salomekbg@gmail.com>
2017-04-10 11:14:27 -0400

    fix code for get_games method in adapter

--- app/adapters/igdb_api.rb
+++ app/adapters/igdb_api.rb
@@ -9,11 +9,18 @@ class IgdbApi
 
     games.body.each do |game|
       if game["release_dates"]
-        Game.find_or_create_by(name: game["name"], year: game["release_dates"][0]["human"][0..3], summary: game["summary"], cover: game["cover"])
+        Game.find_or_create_by(name: game["name"], year: game["release_dates"][0]["human"][0..3], summary: game["summary"], cover: game["cover"]["url"])
       else
-        Game.find_or_create_by(name: game["name"], year: "No Year Available", summary: game["summary"], cover: game["cover"])
+        Game.find_or_create_by(name: game["name"], year: "No Year Available", summary: game["summary"], cover: game["cover"]["url"])
       end
     end
+  end
+
+  def self.get_genres
+    Unirest.get "https://igdbcom-internet-game-database-v1.p.mashape.com/genres/?fields=name&limit=50",
+    headers:{
+      "X-Mashape-Key" => "LC6dJEpGZVmshUoieKLHL6Gumqwwp1rTJmvjsnJhpSSyDsSUd7"
+    }
   end
 
 end
